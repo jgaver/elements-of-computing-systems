46aeea66f3229717896645166d0fa72e39eca368
john-p-g <john.gaver@gmail.com>
2017-05-19 21:17:05 -0400

    Implement fill assembly

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,54 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+@i
+M=0
+
+(LOOP)
+	@i
+	D=M
+	@8192 // 32 x 256
+	D=D-A
+	@RESET
+	D;JEQ
+
+	@KBD
+	D=M
+	@ON
+	D;JGT
+
+	@OFF
+	0;JMP
+
+(INCR)
+	@i
+	M=M+1
+	
+	@LOOP
+	0;JMP
+
+(RESET)
+	@i
+	M=0
+	@LOOP
+	0;JMP
+
+	
+(ON)
+	@i
+	D=M
+	@SCREEN
+	A=D+A
+	M=-1
+	@INCR
+	0;JMP
+
+(OFF)	
+	@i
+	D=M
+	@SCREEN
+	A=D+A
+	M=0
+	@INCR
+	0;JMP
